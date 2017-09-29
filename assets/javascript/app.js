a3fcb80b7a7042c1c665da6e4501eb27ff42bf2c
Winfred Sunga <winfredsunga01@gmail.com>
2017-09-28 20:51:33 -0700

    fixed bugs with gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -39,18 +39,17 @@
   //function to update the correct answers
   //function to update the wrong answers
 
-var timeOutId;
-var intervalId;
+
 
  var triviaQuiz = {
   counter : 0 ,
-  intervalDurationForTrivias : 20000, 
-  timeOutForResultGif : 1000,
-  timeOutForTransitionGif : 2500,
-  timeOutForNextTrivia : 5000,
-  gifCorrect : "../images/wrong.gif",
-  gifWrong :  "../images/wrong.gif",
-  gifTimeIsUp : "../images/timeUp.gif",
+  intervalDurationForQuestions : 3000, //in milliseconds
+  timeOutForResultGif : 1000, //in milliseconds
+  timeOutForTransitionGif : 2500, //in milliseconds
+  timeOutForNextTrivia : 5000, //in milliseconds
+  gifCorrect : "assets/images/correct.gif",
+  gifWrong :  "assets/images/wrong.gif",
+  gifTimeIsUp : "assets/images/timeUp.gif",
   currentQuestion : "",
   currentMultipleChoices : "",
   currentAnswer : "",
@@ -60,31 +59,31 @@ var intervalId;
       question : "Which character was possessed by the demon in the 1973 movie The Exorcist?",
       choices : ["Father Karras", "Regan MacNeil", "Chris McNeil", "Buffy the Vampire Slayer"] ,
       answer : "Regan MacNeil",
-      transitionGif : "../images/spiderWalk.gif",
+      transitionGif : "assets/images/spiderWalk.gif",
     },
     {
       question : "Freddy Krueger's family residential address is 1428 ___ street",
       choices : ["Amytiville", "Palm", "Nightmare", "Elm"] ,
       answer : "Elm",
-      transitionGif : "../images/freddy.gif",
+      transitionGif : "assets/images/freddy.gif",
     },
     {
       question : "Name of the villain in Texas Chainsaw Massacre",
       choices : ["Leatherface", "Oilyface", "Marco from Tropoja", "Chucky"] ,
       answer : "Leatherface",
-      transitionGif : "../images/chainsaw.gif",
+      transitionGif : "assets/images/chainsaw.gif",
     },
     {
       question : "In The Exorcist and The Rite, this famous line was used by the priests to banish the devil",
       choices : ["Leave now and never come back", "The power of Christ compels you", "Go back to wherever you came from", "Say hello to my little friend"] ,
       answer : "The power of Christ compels you",
-      transitionGif : "../images/compelling.gif",
+      transitionGif : "assets/images/compelling.gif",
     },
     {
       question : "Jason is the name of the villain in this movie series",
       choices : ["Scream", "Halloween", "The Ring", "Friday the 13th"] ,
       answer : "Friday the 13th",
-      transitionGif : "../images/jason.gif",
+      transitionGif : "assets/images/jason.gif",
     }
   ], 
   correctAnswersCount : 0,
@@ -101,50 +100,188 @@ var intervalId;
   updateCurrentTransitionGif : function() {
     this.currentgifTransition = this.quizzes[this.counter].transitionGif;
   },
-  updateCorrectAnswersCount : function() {
-
-  },
-  updateWrongAnswersCount : function() {
-    this.correctAnswersCount++;
-  },
-  displayQuiz : function() {
-    this.wrongAnswersCount++;
-  }
  } 
 
+ var timeRemaining; 
+ var timeOutId;
+ var intervalId;
+
  //create a function to display the timer every second
+ //funtion to start the quiz
+ //function to stop the quiz
 
  
  //create a function to display the question
-function displayQuestion() {
+function getQuestion() {
   //first update the appropriate variable in the object
   triviaQuiz.updateCurrentQuestion();
-  //return the question
+  //triviaQuiz.currentQuestion = triviaQuiz.quizzes[triviaQuiz.counter].question;    
+
   return triviaQuiz.currentQuestion;
 }
 
-
-
  //create a function to display the multiple choices
-function displayMultipleChoices() {
-triviaQuiz.updateCurrentMultipleChoices();
-return triviaQuiz.currentMultipleChoices
+function getMultipleChoices() {
+  triviaQuiz.updateCurrentMultipleChoices();
+  //triviaQuiz.currentMultipleChoices = triviaQuiz.quizzes[triviaQuiz.counter].choices;
+
+  return triviaQuiz.currentMultipleChoices;
 }
 
-$(document).ready(function() {
-  var question = displayQuestion();
-  var choice_1 = displayMultipleChoices()[0];
-  var choice_2 = displayMultipleChoices()[1];
-  var choice_3 = displayMultipleChoices()[2];
-  var choice_4 = displayMultipleChoices()[3];
+//create a function to display the timer
+function countdown() {
+  
+  //decrement the time remaining by 1 second after each call to this function
+  timeRemaining--;
 
+  //increment the counter by 1 
+  //triviaQuiz.counter++;
+    $("#timer").text("Time Remaining : " + timeRemaining + " seconds");
+  
+  //we want to stop the countdown and quiz once we reach 0 seconds
+  if(timeRemaining === 0) {
+    //stopQuiz();
+    //showTimeIsOutGif();
+    evaluateAnswer();
+    //timeOutId = setTimeout(showTransitionGif, 2500);
+    //timeOutId = setTimeout(showTransitionGif, triviaQuiz.timeOutForTransitionGif);
+  }
+}
 
-  //on document load, display the first question, the multiple choices, and the timer ticking down from 20s to 0s
+function startQuiz() {
+
+  //first remove the button 
+  $("#start-button").remove();
+
+  timeRemaining = triviaQuiz.intervalDurationForQuestions / 1000;
+    
+  //display the question, the multiple choices, and the timer ticking down from 20s to 0s
+  var question = getQuestion();
+  var choice_1 = getMultipleChoices()[0];
+  var choice_2 = getMultipleChoices()[1];
+  var choice_3 = getMultipleChoices()[2];
+  var choice_4 = getMultipleChoices()[3];
+  //get the answer and store in the appropriate object key
+  triviaQuiz.updateCurrentAnswer();
+
+  $("#timer").text("Time Remaining : " + timeRemaining + " seconds");
   $("#question").text(question);
   $("#choice_1").text("<> " + choice_1);
   $("#choice_2").text("<> " + choice_2);
   $("#choice_3").text("<> " + choice_3);
   $("#choice_4").text("<> " + choice_4);
 
-});
+  //display time left every 1 sev
+  intervalId = setInterval(countdown, 1000);
+  
+}
+
+function stopQuiz() {
+  //this clears out the interval and removes texts
+  clearInterval(intervalId);
+}
+
+//function to clear out the all texts (except headings) 
+function removeAllTexts() {
+  $("#timer").empty();
+  $("#question").empty();
+  $("#choice_1").empty();
+  $("#choice_2").empty();
+  $("#choice_3").empty();
+  $("#choice_4").empty();
+}
+
+function evaluateAnswer() {
+  stopQuiz();
+
+  var answer = $(this).text();
+  var correctAnswer = "<> " + triviaQuiz.currentAnswer;
+
+  console.log("this is : " + $(this));
+  console.log("answer : " + answer);
+  console.log("correct answer : " + correctAnswer);
+  console.log("type of answer : " + typeof(answer));
+
+  removeAllTexts();
+
+  if(answer === correctAnswer) {
+    showCorrectGif();
+    //$("#padding-2").html("<img src='" + triviaQuiz.gifCorrect + "' alt='answer is correct'>");
+    triviaQuiz.correctAnswersCount++;
+    timeOutId = setTimeout(showTransitionGif, 2000);
+  } else if (answer !== correctAnswer && answer !== "") {
+    showWrongGif();
+    triviaQuiz.wrongAnswersCount++;
+    timeOutId = setTimeout(showTransitionGif, 3500);
+  } else {
+    showTimeIsOutGif();
+    triviaQuiz.wrongAnswersCount++;
+    timeOutId = setTimeout(showTransitionGif, 2000);
+  }
+
+/*
+  else if(typeof($(this)) === "object") {
+    showTimeIsOutGif();
+    triviaQuiz.wrongAnswersCount++;
+    timeOutId = setTimeout(showTransitionGif, 2000);
+  } else {
+    showWrongGif();
+    //$("#padding-2").html("<img src='" + triviaQuiz.gifWrong + "' alt='answer is wrong'>");
+    //$("#padding-2").append("<h3>Correct Answer : " + triviaQuiz.currentAnswer + "</h3>");
+    triviaQuiz.wrongAnswersCount++;
+    timeOutId = setTimeout(showTransitionGif, 3500);
+  }
+*/  
+}
+
+function showTransitionGif() {
+  triviaQuiz.updateCurrentTransitionGif()
+  $("#padding-2").html("<img src='" + triviaQuiz.currentgifTransition + "' alt='gif from the movie'>");
+}
+
+function showWrongGif() {
+  $("#padding-2").html("<img src='" + triviaQuiz.gifWrong + "' alt='answer is wrong'>");
+  $("#padding-2").append("<h3>Correct Answer : " + triviaQuiz.currentAnswer + "</h3>");
+}
+
+function showCorrectGif() {
+  $("#padding-2").html("<img src='" + triviaQuiz.gifCorrect + "' alt='answer is correct'>");
+}
+
+function showTimeIsOutGif() {
+  $("#padding-2").html("<img src='" + triviaQuiz.gifTimeIsUp + "' alt='time is up'>");
+  $("#padding-2").append("<h3>Correct Answer : " + triviaQuiz.currentAnswer + "</h3>");
+}
+
+
+//event listeners
+$("#start-button").on("click", startQuiz);
+$("#choice_1").on("click", evaluateAnswer);
+$("#choice_2").on("click", evaluateAnswer);
+$("#choice_3").on("click", evaluateAnswer);
+$("#choice_4").on("click", evaluateAnswer);
+
+// $("#choice_2").on("click", function(event){
+
+// });
+
+
+
+
+
+  
+
+  //var timeLeft = displayTimer();
+  
+  
+
+  
+  // intervalQuestion = setInterval(displayTimer, 1000);
+
+  // setTimeout(clearInterval(intervalQuestion),10000);
+
+
+
+
+
  
